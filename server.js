6c381301b63f843457b083e77f525a06c161c2d7
Robin Lunde <robinnr1@gmail.com>
2019-11-02 15:48:59 +0100

    Better logging. More endpoints

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
 // content of index.js
 const http = require('http')
+const fs = require('fs')
+require('log-timestamp');
 const port = 3366
 var i = 1;
+
 const requestHandler = (request, response) => {
   try{
     var ip = request.headers['x-forwarded-for'] || request.headers['x-real-ip'] || request.connection.remoteAddress;
+    var ip_fw = request.headers['x-forwarded-for'];
+    var ip_real = request.headers['x-real-ip'];
+    /*
     console.log(request.headers['x-forwarded-for']);
     console.log(request.headers['x-real-ip']);
     console.log(request.connection.remoteAddress);
+    */
   }catch(e){
     console.error("Error!", e);
     var ip = "Not found!";
   }
   var cookie = request.headers['cookie'] || "None";
-  console.log("Request nr: " + i++ + " URL: "+request.url + " IP: " + ip + " Cookies: \n" + cookie);
-  response.writeHead(200);//, {'Content-Type': 'image/jpeg'} );
-  response.end("ok\n");//fs.readFileSync(__dirname+'/img.jpg'));
+  console.log("\n[+] Request nr: " + i++ + "\n	[*] URL: "+request.url + "\n	[*] Cookies: " + cookie+ "\n	[*] IP: " + ip + " Fw_IP: " + ip_fw + " Real_ip: " + ip_real + "\n");
+  if(request.url == "/img"){
+	  response.writeHead(200, {"Content-Type": "image/jpeg"});
+	  response.write(fs.readFileSync(__dirname+'/Panda_hut.jpg'));
+	  response.end();
+  }else{
+	  response.writeHead(200, {"Content-Type": "text/html"});
+	  response.write(fs.readFileSync(__dirname+'/base.html'));
+	  response.end();
+  }
 }
 
 const server = http.createServer(requestHandler)
